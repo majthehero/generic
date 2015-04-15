21852a6feb14745fca9712547d6a7baca70950c9
Maj Smerkol <maj.smerkol@gmail.com>
2015-04-16 00:49:40 +0200

    odpiranje dira dela, naslednji korak list all entries

---
+++ fm/.vimrc
@@ -0,0 +1,20 @@
+version 6.0
+if &cp | set nocp | endif
+let s:cpo_save=&cpo
+set cpo&vim
+vmap gx <Plug>NetrwBrowseXVis
+nmap gx <Plug>NetrwBrowseX
+vnoremap <silent> <Plug>NetrwBrowseXVis :call netrw#BrowseXVis()
+nnoremap <silent> <Plug>NetrwBrowseX :call netrw#BrowseX(expand((exists("g:netrw_gx")? g:netrw_gx : '<cfile>')),netrw#CheckIfRemote())
+let &cpo=s:cpo_save
+unlet s:cpo_save
+set backspace=indent,eol,start
+set cindent
+set fileencodings=ucs-bom,utf-8,default,latin1
+set helplang=en
+set ruler
+set shiftwidth=4
+set smartindent
+set suffixes=.bak,~,.swp,.o,.info,.aux,.log,.dvi,.bbl,.blg,.brf,.cb,.ind,.idx,.ilg,.inx,.out,.toc,.png,.jpg
+set tabstop=4
+" vim: set ft=vim :
---
+++ fm/proj3.c
@@ -0,0 +1,53 @@
+//#include <fs/open.h>
+#include <stdio.h>
+#include <errno.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <dirent.h>
+#include <sys/types.h>
+
+#define BUFF_SIZE 256
+
+int main(int argc, char* argv[])
+{
+	char* dir_working = NULL;
+	int _status = 0;
+	if (!getcwd(dir_working, BUFF_SIZE))
+	{
+		perror(sys_errlist[errno]);
+	}
+
+	printf("CWD: %s\n", dir_working);
+	
+	if (argc < 2)
+	{
+		printf("Run %s as %s <path>\nNot enough arguments",
+				argv[0], argv[0]);
+		dir_working = "/";
+	} 
+	else
+	{
+		dir_working = argv[1];
+	}
+	if (chdir(dir_working))
+	{
+		perror(sys_errlist[errno]);		
+		exit(2);
+	}
+
+	printf("CWD: %s\n", dir_working);
+
+	// list dirents
+	DIR* dirp_working = NULL;
+	if (dirp_working = opendir(dir_working) == NULL)
+	{
+		perror(sys_errlist[errno]);
+		exit(3);
+	}
+	struct dirent * = malloc(sizeof(struct dirent));
+	if (readdir(dirp_working))
+
+
+	printf("kthxbye\n");
+}
+
