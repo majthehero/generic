7295373580021be03858e474cbda07cf59ac04a3
Maj Smerkol <maj.smerkol@gmail.com>
2015-06-01 10:11:46 +0200

    začetek dela na izzivu 11 - bellman-ford, iskanje najcenejših poti

---
+++ Izziv11/Izziv11.java
@@ -0,0 +1,30 @@
+public class Izziv11 {
+
+	/**
+	main: argument je število vozlišč
+	*/
+	public static void main(String[] args) {
+
+		// instanca s pravimi podatki
+
+		// preberi vhod
+
+		// reši
+
+		// izpiši
+
+	}
+	/**
+	members
+	*/
+	int n_vozl;
+	int[][] povezave;
+
+	/**
+	konstruktor
+	*/
+	public Izziv11 (int n_vozl) {
+		this.n_vozl = n_vozl;
+	}
+
+}
